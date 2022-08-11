6d6f7593e8c2d6e84e93a0aa5c18d6aae47b30ae
Adam Selph <adam@Adams-MBP.fios-router.home>
2022-08-11 14:27:47 -0400

    added api integration and more quoter settings

--- script.js
+++ script.js
@@ -7,7 +7,11 @@ const image = document.createElement("img");
 image.id = 'shrekFace';
 const shrekFace = document.querySelector('#shrekFace');
 body.appendChild(image);
-
+const pirateUrl = "url(https://p.kindpng.com/picc/s/156-1569003_jack-sparrow-transparent-png-png-download.png)";
+const shrekUrl = "url(https://pngimg.com/uploads/shrek/small/shrek_PNG6.png)";
+const abeUrl = "url(https://cdn.britannica.com/34/78134-050-3AC7D4F8/Abraham-Lincoln-photograph-Mathew-Brady.jpg?w=400&h=300&c=crop)"
+const shakespeareUrl = "url(https://www.history.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTU3ODc5MDg3NTExNDQ2ODU3/shakespeare-hero.jpg)";
+const buddhaUrl = "url(https://c4.wallpaperflare.com/wallpaper/558/595/705/religious-buddhism-wallpaper-preview.jpg)";
 
 const shrekQuotes = [
   "Get off of me swamp!",
@@ -15,9 +19,7 @@ const shrekQuotes = [
   "Well it\'s no wonder you don\'t have any friends.",
   "Onions have layers, Ogres have layers.",
   "Somebody once told me the world is gonna roll me...",
-  "That'll do Donkey, that'll do.",
   "Donkey, you have the right to remain silent. What you lack is the capacity.",
-  "Squeeze the jelly from your eyes! Actually, it\'s quite good on toast.",
   "Well, That\'s Not Very Nice. It\'s Just A Donkey.",
   "Do you think maybe he's compensating for something?",
   "Hey! I'm no one's messenger boy, all right? I'm a delivery boy.",
@@ -38,6 +40,38 @@ const pirateQuotes = [
   "This Shot Is Not Meant For You.",
 ];
 
+const abeQuotes = [];
+
+const shakespeareQuotes = [];
+
+const buddhaQuotes = [];
+
+
+let cache;
+fetch('https://type.fit/api/quotes')
+  .then((data) => data.json())
+  .then((data) => {
+    cache = data;
+    console.log(cache);
+    // iterate through data, pull all abe lincoln quotes
+    for(let i = 0; i < cache.length; i++) {
+      if(cache[i]['author'] === 'Abraham Lincoln') {
+        abeQuotes.push(cache[i]['text']);
+      }
+      if(cache[i]['author'] === 'William Shakespeare') {
+        shakespeareQuotes.push(cache[i]['text']);
+      }
+      if(cache[i]['author'] === 'Buddha') {
+        buddhaQuotes.push(cache[i]['text']);
+      }
+    }
+    console.log(abeQuotes);
+  })
+  .catch((err) => {
+    console.log('Error: ', err)
+  });
+
+
 let quotes = shrekQuotes;
 
 
@@ -56,17 +90,17 @@ body.appendChild(quote);
 setTimeout(enterQuote, 2000);
 
 
-function enterQuote () {
+function enterQuote() {
   let opacity = Number(body.querySelector('#quote').style.opacity);
   if (opacity < 1) {
     opacity += 0.1;
     body.querySelector('#quote').style.opacity = opacity.toString();
     body.querySelector('#quoteBox').style.opacity = opacity.toString();
     setTimeout(enterQuote, 50);
-  } else setTimeout(exitQuote, 4000);
+  } else setTimeout(exitQuote, 3000);
 }
 
-function exitQuote () {
+function exitQuote() {
   let opacity = Number(body.querySelector('#quote').style.opacity);
   if (opacity > 0) {
     opacity -= 0.1;
@@ -75,8 +109,10 @@ function exitQuote () {
     setTimeout(exitQuote, 50);
   }
   else {
-    body.querySelector("#quote").innerHTML = quotes[Math.floor(Math.random() * quotes.length)]
-    setTimeout(enterQuote, 2000);
+    setTimeout(function () {
+      body.querySelector("#quote").innerHTML = quotes[Math.floor(Math.random() * quotes.length)];
+      enterQuote();
+    }, 800);
   }
 }
 
@@ -95,16 +131,41 @@ function exitQuote () {
 // shrekFace.style.onclick = function() {
 //   clicked();
 // }
+let currentHead = shrekUrl;
 function clicked() {
-  document.querySelector('#shrekFace').style.content =  "url(https://p.kindpng.com/picc/s/156-1569003_jack-sparrow-transparent-png-png-download.png";
-  quotes = pirateQuotes;
-  console.log("Quotes should be changed to pirates");
+  if (currentHead === shrekUrl) {
+    document.querySelector('#shrekFace').style.content = pirateUrl;
+    quotes = pirateQuotes;
+    currentHead = pirateUrl;
+  }
+  else if (currentHead === pirateUrl) {
+    document.querySelector('#shrekFace').style.content = abeUrl;
+    quotes = abeQuotes;
+    currentHead = abeUrl;
+  }
+  else if (currentHead === abeUrl) {
+    document.querySelector('#shrekFace').style.content = shakespeareUrl;
+    quotes = shakespeareQuotes;
+    currentHead = shakespeareUrl;
+  }
+  else if (currentHead === shakespeareUrl) {
+    document.querySelector('#shrekFace').style.content = buddhaUrl;
+    quotes = buddhaQuotes;
+    currentHead = buddhaUrl;
+  }
+  else {
+    document.querySelector('#shrekFace').style.content = shrekFace;
+    quotes = shrekQuotes;
+    currentHead = shrekFace;
+  }
+
 }
 // body.addEventListener('keydown', (e) => {
 //   if (e.code === 'ArrowDown') {
 //     body.querySelector('#shrekFace').style.content =  "url(https://p.kindpng.com/picc/s/156-1569003_jack-sparrow-transparent-png-png-download.png";
 //   }
 // });
-document.querySelector('#shrekFace').addEventListener('click', function(e){
+document.querySelector('#shrekFace').addEventListener('click', function (e) {
   clicked();
-});
+  console.log(cache);
+})
